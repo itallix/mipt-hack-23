a86c72ed1c1bb09dfba7cec065ad674c5d06bef9
Zagir <zainudinov.zagir@gmail.com>
2023-12-25 01:09:53 +0300

    Default prompt has been changed in story.py, english language has been set up as default

--- bot_service/handlers/story.py
+++ bot_service/handlers/story.py
@@ -4,8 +4,8 @@ from telegram.ext import ContextTypes
 from vertexai.preview.generative_models import GenerationConfig, GenerativeModel
 
 DEFAULT_PROMPT = {
-    "ru": "Расскажи мне какую-нибудь историю",
-    "en": "Tell me a random story",
+    "ru": "Расскажи мне забавный факт о случайном городе",
+    "en": "Tell me a fun fact about some random city",
 }
 
 
@@ -17,7 +17,7 @@ async def handle_random_story(update: Update, context: ContextTypes.DEFAULT_TYPE
     msg = update.message
     await context.bot.send_chat_action(chat_id=msg.chat_id, action=ChatAction.TYPING)
     model = GenerativeModel('gemini-pro')
-    prompt = DEFAULT_PROMPT[msg.from_user.language_code]
+    prompt = DEFAULT_PROMPT.get(msg.from_user.language_code, "en")
     response = model.generate_content(
                 prompt,
                 generation_config=GenerationConfig(
