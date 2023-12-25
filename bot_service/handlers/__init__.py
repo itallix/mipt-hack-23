426b5df9bad8f219cd850b06a13295af87456f3b
Vitalii Karniushin <itallix@me.com>
2023-12-25 17:00:51 +0100

    chore: Linting

--- bot_service/handlers/__init__.py
+++ bot_service/handlers/__init__.py
@@ -1,6 +1,11 @@
 from .photo import handle_photo_message
 from .start import show_intro
-from .voice import handle_voice_message
 from .story import handle_random_story
+from .voice import handle_voice_message
 
-__all__ = ["show_intro", "handle_voice_message", "handle_photo_message", "handle_random_story"]
+__all__ = [
+    "show_intro",
+    "handle_voice_message",
+    "handle_photo_message",
+    "handle_random_story",
+]
